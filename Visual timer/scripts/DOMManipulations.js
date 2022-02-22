cc6b041e4751a0a18167ef08018be06345a9e822
amakor97 <amakor.andrei@gmail.com>
2022-02-22 14:31:06 +0300

    UI for adding timer by set up amounts of time units is ready

--- Visual timer/scripts/DOMManipulations.js
+++ Visual timer/scripts/DOMManipulations.js
@@ -113,7 +113,7 @@ export function fillSettingUpTimer(settingUpTimer) {
   
 
   let inputFinishType2Label = document.createElement("label");
-  inputFinishType2Label.textContent = "Precise finish time";
+  inputFinishType2Label.textContent = "Amount of different units";
   finishTypeFieldset.appendChild(inputFinishType2Label);
   let forAttrFinish2 = document.createAttribute("for");
   forAttrFinish2.value = `finishType2`;
@@ -126,6 +126,20 @@ export function fillSettingUpTimer(settingUpTimer) {
   inputFinishType2Label.appendChild(inputFinishType2);
 
   
+  let inputFinishType3Label = document.createElement("label");
+  inputFinishType3Label.textContent = "Precise finish time";
+  finishTypeFieldset.appendChild(inputFinishType3Label);
+  let forAttrFinish3 = document.createAttribute("for");
+  forAttrFinish3.value = `finishType3`;
+  inputFinishType3Label.setAttributeNode(forAttrFinish3);
+
+  let inputFinishType3 = document.createElement("input");
+  inputFinishType3.type = "radio";
+  inputFinishType3.name = "finishType";
+  inputFinishType3.id = "finishType3";
+  inputFinishType3Label.appendChild(inputFinishType3);
+
+  
   finishTypeFieldset = document.createElement("fieldset");
   finishTypeFieldset.classList.add("add-form__fieldset");
   addForm.appendChild(finishTypeFieldset);
@@ -134,17 +148,105 @@ export function fillSettingUpTimer(settingUpTimer) {
   inputNumberLabel.textContent = "Please, enter the amount of seconds";
   finishTypeFieldset.appendChild(inputNumberLabel);
   let forAttr = document.createAttribute("for");
-  forAttr.value = `InputSecondsAmount`;
+  forAttr.value = `InputOnlySecondsAmount`;
   inputNumberLabel.setAttributeNode(forAttr);
 
   let inputNumber = document.createElement("input");
   inputNumber.classList.add("add-form__input-time", "js-input-number");
   inputNumber.type = "number";
   inputNumber.min = "0";
-  inputNumber.id = `InputSecondsAmount`;
+  inputNumber.id = `InputOnlySecondsAmount`;
   inputNumberLabel.appendChild(inputNumber);
 
 
+  
+  let inputDaysLabel = document.createElement("label");
+  inputDaysLabel.textContent = "Days";
+  finishTypeFieldset.appendChild(inputDaysLabel);
+  forAttr = document.createAttribute("for");
+  forAttr.value = "InputDaysAmount";
+  inputDaysLabel.setAttributeNode(forAttr);
+
+  let inputDays = document.createElement("input");
+  inputDays.classList.add("add-form__input-amount", "js-input-days");
+  inputDays.type = "number";
+  inputDays.disabled = true;
+  inputDays.min = "0";
+  inputDays.addEventListener("input", function() {
+    if (this.value < 0) {
+      this.value = 0;
+    }
+  })
+  inputDays.id = `InputDaysAmount`;
+  inputDaysLabel.appendChild(inputDays);
+
+
+  let inputHoursLabel = document.createElement("label");
+  inputHoursLabel.textContent = "Hours";
+  finishTypeFieldset.appendChild(inputHoursLabel);
+  forAttr = document.createAttribute("for");
+  forAttr.value = "InputHoursAmount";
+  inputHoursLabel.setAttributeNode(forAttr);
+
+  let inputHours = document.createElement("input");
+  inputHours.classList.add("add-form__input-amount", "js-input-hours");
+  inputHours.type = "number";
+  inputHours.disabled = true;
+  inputHours.min = "0";
+  inputHours.max = "23";
+  inputHours.addEventListener("input", function() {
+    if ((this.value < 0) || (this.value > 59)) {
+      this.value = 0;
+    }
+  })
+  inputHours.id = `InputHoursAmount`;
+  inputHoursLabel.appendChild(inputHours);
+
+
+  let inputMinsLabel = document.createElement("label");
+  inputMinsLabel.textContent = "Mins";
+  finishTypeFieldset.appendChild(inputMinsLabel);
+  forAttr = document.createAttribute("for");
+  forAttr.value = "InputMinsAmount";
+  inputMinsLabel.setAttributeNode(forAttr);
+
+  let inputMins = document.createElement("input");
+  inputMins.classList.add("add-form__input-amount", "js-input-mins");
+  inputMins.type = "number";
+  inputMins.disabled = true;
+  inputMins.min = "0";
+  inputMins.max = "59";
+  inputMins.addEventListener("input", function() {
+    if ((this.value < 0) || (this.value > 59)) {
+      this.value = 0;
+    }
+  })
+  inputMins.id = `InputMinsAmount`;
+  inputMinsLabel.appendChild(inputMins);
+
+
+  let inputSecsLabel = document.createElement("label");
+  inputSecsLabel.textContent = "Secs";
+  finishTypeFieldset.appendChild(inputSecsLabel);
+  forAttr = document.createAttribute("for");
+  forAttr.value = "InputSecsAmount";
+  inputSecsLabel.setAttributeNode(forAttr);
+
+  let inputSecs = document.createElement("input");
+  inputSecs.classList.add("add-form__input-amount", "js-input-secs");
+  inputSecs.type = "number";
+  inputSecs.disabled = true;
+  inputSecs.min = "0";
+  inputSecs.max = "59";
+  inputSecs.addEventListener("input", function() {
+    if ((this.value < 0) || (this.value > 59)) {
+      this.value = 0;
+    }
+  })
+  inputSecs.id = `InputSecsAmount`;
+  inputSecsLabel.appendChild(inputSecs);
+
+  
   let inputFinishLabel = document.createElement("label");
   inputFinishLabel.textContent = "Please, enter finish datetime";
   finishTypeFieldset.appendChild(inputFinishLabel);
@@ -152,13 +254,13 @@ export function fillSettingUpTimer(settingUpTimer) {
   forAttr.value = `InputFinishTime`;
   inputFinishLabel.setAttributeNode(forAttr);
 
-
   let inputFinishTime = document.createElement("input");
   inputFinishTime.classList.add("add-form__input-time", "js-input-finish-time");
   inputFinishTime.type = "datetime-local";
   inputFinishTime.id = `InputFinishTime`;
   inputFinishTime.disabled = true;
   inputFinishLabel.appendChild(inputFinishTime);
+
 
   let dataSubmitBtn = document.createElement("button");
   dataSubmitBtn.classList.add("add-form__submit", "btn");
@@ -178,12 +280,43 @@ export function fillSettingUpTimer(settingUpTimer) {
 
   inputFinishType1.addEventListener("input", function() {
     inputFinishTime.value = "";
+    inputDays.value = "";
+    inputHours.value = "";
+    inputMins.value = "";
+    inputSecs.value = "";
+
     inputNumber.disabled = false;
+    inputDays.disabled = true;
+    inputHours.disabled = true;
+    inputMins.disabled = true;
+    inputSecs.disabled = true;
     inputFinishTime.disabled = true;
   })
+
   inputFinishType2.addEventListener("input", function() {
+    inputFinishTime.value = "";
     inputNumber.value = "";
+
     inputNumber.disabled = true;
+    inputDays.disabled = false;
+    inputHours.disabled = false;
+    inputMins.disabled = false;
+    inputSecs.disabled = false;
+    inputFinishTime.disabled = true;
+  })
+
+  inputFinishType3.addEventListener("input", function() {
+    inputNumber.value = "";
+    inputDays.value = "";
+    inputHours.value = "";
+    inputMins.value = "";
+    inputSecs.value = "";
+
+    inputNumber.disabled = true;
+    inputDays.disabled = true;
+    inputHours.disabled = true;
+    inputMins.disabled = true;
+    inputSecs.disabled = true;
     inputFinishTime.disabled = false;
   })
 
