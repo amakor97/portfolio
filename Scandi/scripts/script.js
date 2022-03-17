bcb3c9cb27b9aa8c63fdd7357c0d4e83462c1248
amakor97 <amakor.andrei@gmail.com>
2022-03-17 12:36:16 +0300

    SC desktop layout is ready

--- Scandi/scripts/script.js
+++ Scandi/scripts/script.js
@@ -4,16 +4,34 @@
 //setting up overlays on hovers events over works containers
 
 let workWrappers = document.querySelectorAll(".js-work-wrapper");
-console.log(workWrappers);
 
 workWrappers.forEach(function(wrapper) {
-    let overlay = wrapper.querySelector(".js-wrapper-overlay");
+  let overlay = wrapper.querySelector(".js-wrapper-overlay");
+  
   if (overlay) {
     wrapper.addEventListener("mouseover", function() {
       overlay.classList.add("works__img-overlay_visible");
     })
     wrapper.addEventListener("mouseleave", function() {
       overlay.classList.remove("works__img-overlay_visible");
+    })
+  }
+})
+
+
+//change review arrows color on hover
+
+let arrowBtns = document.querySelectorAll(".reviews__arrow");
+
+arrowBtns.forEach(function(btn) {
+  let svgArrow = btn.querySelector(".reviews__arrow-svg");
+  
+  if (svgArrow) {
+    btn.addEventListener("mouseover", function() {
+      svgArrow.classList.add("reviews__arrow-svg_hover");
+    })
+    btn.addEventListener("mouseleave", function() {
+      svgArrow.classList.remove("reviews__arrow-svg_hover");
     })
   }
 })
