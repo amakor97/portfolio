ec30ec340372609ae2b29fa5db5f5f2938aa6921
amakor97 <amakor.andrei@gmail.com>
2022-04-28 17:03:07 +0300

    MA make catalog page filter section layout for smartphones

---
+++ Maroon/src/js/filter.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const toggleFilterBtns = document.querySelectorAll(".js-toggle-filter-btn");
+const filterBody = document.querySelector(".js-filter");
+
+toggleFilterBtns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    filterBody.classList.toggle("filter--showed");
+  })
+})
+
+
+const toggleFieldsetsBtns = document.querySelectorAll(".js-toggle-fieldset-btn");
+
+toggleFieldsetsBtns.forEach(function(btn) {
+  btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    this.classList.toggle("filter__accordion-btn--opened");
+
+    let panel = this.nextElementSibling;
+    //console.log(panel);
+    if (!panel.classList.contains("filter__fieldset--opened")) {
+      panel.classList.add("filter__fieldset--opened");
+    } else {
+      panel.classList.remove("filter__fieldset--opened");
+    }
+  })
+})
+
+
+
+const applyBtn = document.querySelector(".js-apply-filter");
+const filterCheckboxes = document.querySelectorAll(".js-filter-checkbox");
+applyBtn.addEventListener("click", function() {
+  filterCheckboxes.forEach(function(input) {
+    if (input.checked === true) {
+      console.log(input);
+    }
+  })
+})
