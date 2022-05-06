a68c0fb3cca8faeaed50a9d7bfab1998cb155865
amakor97 <amakor.andrei@gmail.com>
2022-05-06 19:10:34 +0300

    MA make catalog and position pages max-width constraints

--- Maroon/src/js/filter-control-btns.js
+++ Maroon/src/js/filter-control-btns.js
@@ -4,15 +4,44 @@ window.addEventListener("DOMContentLoaded", btnsBlockPosHandler)
 window.addEventListener("resize", btnsBlockPosHandler);
 
 function btnsBlockPosHandler() {
+  if (this.screen.width >= 1630) {
+    let fieldsetPos = getThirdFieldsetPos();
+    let formPos = getFilterContentPos();
+    let elemPos = fieldsetPos.left - formPos.left;
+    console.log("fieldPos", fieldsetPos.left);
+    console.log("formPos", formPos.left);
+
+    let formObj = document.querySelector(".js-filter-content");
+    let formSyleObj = window.getComputedStyle(formObj);
+    console.log("margin left", formSyleObj.marginLeft);
+
+    console.log({elemPos});
+    elemPos += parseInt(formSyleObj.marginLeft);
+    console.log({elemPos});
+    setBtnslBlockPos(elemPos);
+    return;
+  }
   if (this.screen.width >= 1366) {
-    let elemPos = getThirdFieldsetPos();
-    setBtnslBlockPos(elemPos.left);
+    let fieldsetPos = getThirdFieldsetPos();
+    let formPos = getFilterContentPos();
+    let elemPos = fieldsetPos.left - formPos.left;
+    console.log("fieldPos", fieldsetPos.left);
+    console.log("formPos", formPos.left);
+    console.log(elemPos);
+    setBtnslBlockPos(fieldsetPos.left);
   }
 }
 
 function getThirdFieldsetPos() {
   const thirdFieldSet = document.getElementsByClassName("js-filter-fieldset")[2];
   let pos = thirdFieldSet.getBoundingClientRect();
+  console.log(pos.left);
+  return pos;
+}
+
+function getFilterContentPos() {
+  const filterContent = document.querySelector(".js-filter-content");
+  let pos = filterContent.getBoundingClientRect();
   return pos;
 }
 
