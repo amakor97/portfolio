5e155bf0cc3d7a2e49522f1e48afc186988525f2
amakor97 <amakor.andrei@gmail.com>
2022-05-09 16:21:35 +0300

    MA make interactivity for all pages

--- Maroon/src/js/also-carousel.js
+++ Maroon/src/js/also-carousel.js
@@ -8,8 +8,11 @@ const alsoCarousel = document.querySelector(".js-also-carousel-inner");
 const alsoCurrentPageElem = document.querySelector(".js-also-carousel-current-page");
 const alsoMaxPageElem = document.querySelector(".js-also-carousel-max-page");
 
-let alsoData = 4; //length of array of objects
-let alsoDisplayedCards = 1;
+let fullScreenWidth = screen.width;
+console.log({fullScreenWidth});
+
+let alsoData = 8; //length of array of objects
+let alsoDisplayedCards = (fullScreenWidth >= 768) ? 4 : 1;
 let alsoPageNumber = Math.ceil(alsoData / alsoDisplayedCards);
 alsoMaxPageElem.textContent = alsoPageNumber;
 
