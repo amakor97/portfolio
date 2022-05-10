f1c2ff6352e2e6fe9daa2a9d1703ef66b49893e5
amakor97 <amakor.andrei@gmail.com>
2022-05-10 13:05:50 +0300

    MA make intro carousel dynamic

---
+++ Maroon/src/js/fillers/bestsellers-filler.js
@@ -0,0 +1,49 @@
+"use strict";
+
+export function createBestsellersCard(productData) {
+  let card = document.createElement("a");
+  card.classList.add("intro-card", "intro-carousel__intro-card");
+  card.setAttribute("href", productData.href);
+
+  let img = createImg(productData);
+  card.appendChild(img);
+
+  let description = createDescription(productData);
+  card.appendChild(description);
+
+  return card;
+}
+
+function createImg(productData) {
+  let img = document.createElement("img");
+  img.src = productData.srcB;
+  img.classList.add("intro-card__img");
+  img.alt = productData.alt;
+  return img;
+}
+
+function createDescription(productData) {
+  let cont = document.createElement("div");
+  cont.classList.add("intro-card__description");
+
+  let name = document.createElement("p");
+  name.classList.add("intro-card__name");
+  name.textContent = productData.name;
+  cont.appendChild(name);
+
+  let type = document.createElement("p");
+  type.classList.add("intro-card__type");
+  type.textContent = productData.type;
+  cont.appendChild(type);
+
+  let link = document.createElement("p");
+  link.classList.add("intro-card__fake-link");
+  link.textContent = "Подробнее";
+  cont.appendChild(link);
+
+  return cont;
+}
+
+//create basic layout
+// fill it with content
+// return card
--- Maroon/src/js/intro-carousel.js
+++ Maroon/src/js/intro-carousel.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { createBestsellersCard } from "./fillers/bestsellers-filler.js";
+
 const introPrevBtn = document.querySelector(".js-intro-carousel-prev-btn");
 const introNextBtn = document.querySelector(".js-intro-carousel-next-btn");
 const introCarousel = document.querySelector(".js-intro-carousel-inner");
@@ -35,3 +37,22 @@ introPrevBtn.addEventListener("click", function() {
     leftCard--;
   }
 })
+
+
+
+let dataFile = "../../data/productData.json";
+let dataObj = undefined;
+
+fetch (dataFile)
+.then(response => response.json())
+.then( function(json) {
+  dataObj = json;
+  console.log(dataObj);
+  const main = document.querySelector(".main");
+  for (let i = 0; i < cardsNumber; i++) {
+    let card = createBestsellersCard(dataObj[i]);
+    main.appendChild(card);
+    introCarousel.appendChild(card);
+  }
+});
+
