fefb79b8194836ae9aaf1f693c8ba7643e43adf6
amakor97 <amakor.andrei@gmail.com>
2022-05-19 16:13:21 +0300

    MA refactor filter script

--- Maroon/src/js/filter.js
+++ Maroon/src/js/filter.js
@@ -10,33 +10,27 @@ toggleFilterBtns.forEach(function(btn) {
 })
 
 
-const toggleFieldsetsBtns = document.querySelectorAll(".js-toggle-fieldset-btn");
+const toggleFieldsetsBtns = 
+  document.querySelectorAll(".js-toggle-fieldset-btn");
 
 toggleFieldsetsBtns.forEach(function(btn) {
   btn.addEventListener("click", function(e) {
     e.preventDefault();
     this.classList.toggle("accordion-btn--opened");
-
     let panel = this.nextElementSibling;
-    //console.log(panel);
-    if (!panel.classList.contains("filter__fieldset--opened")) {
-      panel.classList.add("filter__fieldset--opened");
-    } else {
-      panel.classList.remove("filter__fieldset--opened");
-    }
+
+    panel.classList.toggle("filter__fieldset--opened", 
+    !panel.classList.contains("filter__fieldset--opened"));
   })
 })
 
 
-
 const applyBtn = document.querySelector(".js-apply-filter");
 const filterCheckboxes = document.querySelectorAll(".js-filter-checkbox");
-applyBtn.addEventListener("click", function() {
-  console.log(dataObj);
+
+applyBtn.addEventListener("click", function(e) {
+  e.preventDefault();
   filterCheckboxes.forEach(function(input) {
-    let faceFilter = document.querySelectorAll(".js-face-filter-checkbox");
-    console.log(faceFilter);
-  
     if (input.checked === true) {
       console.log(input.name);
     }
@@ -45,19 +39,12 @@ applyBtn.addEventListener("click", function() {
 
 
 const filterFieldsets = document.querySelectorAll(".js-filter-fieldset");
-//console.log(filterFieldsets);
 
 window.addEventListener("resize", function() {
   if (this.screen.width >= 768) {
-    //console.log(screen.width);
     for (let i = 0; i < filterFieldsets.length; i++) {
       toggleFieldsetsBtns[i].classList.remove("accordion-btn--opened");
       filterFieldsets[i].classList.remove("filter__fieldset--opened");
     }
   }
 })
-
-
-function filterData(data) {
-
-}
