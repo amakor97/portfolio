17150397e962507a5f4585c153a016ae775996fc
amakor97 <amakor.andrei@gmail.com>
2022-08-03 10:35:36 +0300

    hotfix(TD): recover kinopoisk header files

---
+++ kinopoisk/src/layout/Header/Header.js
@@ -0,0 +1,51 @@
+import "../../Sass/_reset.sass";
+import "../../Sass/_root.sass";
+import "./_header.sass";
+import "../../Sass/mixins/_link-btn.sass";
+import Logo from "../../components/Logo/Logo";
+
+import { useState } from "react";
+
+import AboutWindow from "../../components/AboutWindow/AboutWindow";
+
+function Header() {
+  const [aboutIsOpen, setAboutIsOpen] = useState(false);
+
+  const setAboutIsOpenToTrue = () => {
+    setAboutIsOpen(true);
+  }
+
+  const setAboutIsOpenToFalse = () => {
+    setAboutIsOpen(false);
+  }
+
+  return(
+    <>
+      <div className="Header">
+        <div className="Header__container">
+          <div className="Header__logo-wrapper">
+            <Logo />
+          </div>
+          <ul className="Header__list">
+            <li className="Header__li">
+              <button className="link-btn Header__btn" onClick={setAboutIsOpenToTrue}>
+                О приложении
+              </button>
+            </li>
+            <li className="Header__li">
+              <nav className="Header__nav">
+                <a className="link-btn Header__nav-link" href="/">На главную</a>
+              </nav>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {aboutIsOpen &&
+        <AboutWindow closeAbout={setAboutIsOpenToFalse}/>
+      }
+    </>
+  )
+}
+
+export default Header;
