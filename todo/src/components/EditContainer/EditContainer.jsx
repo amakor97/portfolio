272bf330be2205cd61e774caec87c49f213e411b
amakor97 <amakor.andrei@gmail.com>
2022-08-04 17:12:36 +0300

    feat(TD): task descriptions are now able to be saved and read

--- todo/src/components/EditContainer/EditContainer.jsx
+++ todo/src/components/EditContainer/EditContainer.jsx
@@ -6,9 +6,11 @@ function EditContainer(props) {
   //console.log(props.task.text);
 
   console.log("PROPS", props);
+  console.log("Props task desc", props.task.desc);
 
   const [taskName, setTaskName] = useState(props.task.text);
   const [currentId, setCurrentId] = useState(-1);
+  const [taskDesc, setTaskDesc] = useState(props.task.desc);
   const [isReseted, setIsReseted] = useState(false);
 
   //const [isEditing, setIsEditing] = useState(false);
@@ -22,25 +24,28 @@ function EditContainer(props) {
       console.log("current task name:", props.task.text);
       setCurrentId(props.task.id);
       setTaskName(props.task.text);
+      setTaskDesc(props.task.desc);
     } 
   } else {
     if (isReseted === false) {
       setTaskName('');
+      setTaskDesc('');
       setCurrentId(Date.now());
       console.log(currentId);
       setIsReseted(true);
     }
-
   }
 
 
   function handleSubmit(event) {
-    event.preventDefault()
-    console.log(event.target[1].value)
+    event.preventDefault();
+    console.log(event.target[1].value);
+    console.log(event.target[2].value);
 
     let taskData = {
       id: currentId,
-      text: event.target[1].value
+      text: event.target[1].value,
+      desc: event.target[2].value
     }
 
     console.log(taskData);
@@ -52,6 +57,11 @@ function EditContainer(props) {
     setIsReseted(false);
   }
 
+  function handleMessageChange(e) {
+    setTaskDesc(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <div className="editContainer">
       {
@@ -60,7 +70,7 @@ function EditContainer(props) {
           <fieldset className="editContainer__fieldset">
             <span>{currentId}</span>
             <input className="editContainer__input" type="text" name="taskName" value={taskName} onChange={e => setTaskName(e.target.value)}></input>
-            <textarea className="editContainer__textarea">{props.task.description}</textarea>
+            <textarea className="editContainer__textarea" value={taskDesc} onChange={(e) => handleMessageChange(e)}></textarea>
             <button type="submit">Сохранить</button>
           </fieldset>
         </form>
