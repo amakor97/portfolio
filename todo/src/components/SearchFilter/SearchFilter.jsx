03d9b7a1d8ae300f026fe6b749c244eaf4192ce7
amakor97 <amakor.andrei@gmail.com>
2022-08-10 13:30:27 +0300

    feat(TD): add reset btn to searchFilter component

--- todo/src/components/SearchFilter/SearchFilter.jsx
+++ todo/src/components/SearchFilter/SearchFilter.jsx
@@ -3,13 +3,22 @@ import "./_searchFilter.sass";
 function SearchFilter(props) {
 
   function handleInput(e) {
-    console.log(e.target.value);
     props.setSearchRegEx(e.target.value);
   }
 
+  function resetInput(e) {
+    props.setSearchRegEx(".*");
+    e.target.parentNode["searchReg"].value = "";
+  }
+
   return (
-    <input className="searchFilter" 
-    onChange={(e) => handleInput(e)}></input>
+    <div className="searchFilter">
+      <form>
+        <input className="searchFilter__input" name="searchReg"
+        onChange={(e) => handleInput(e)}></input>
+        <button type="button" className="searchFilter__reset-btn" onClick={(e) => {resetInput(e)}}>Очис</button>
+      </form>
+    </div>
   )
 }
 
