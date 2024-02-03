640e38850d0596d073f8f33691ef3388e69c0206
amakor97 <amakor.andrei@gmail.com>
2024-02-03 20:26:53 +0300

    [FEAT] piano: add sound change function

--- piano/src/js/kbdHandler.js
+++ piano/src/js/kbdHandler.js
@@ -4,27 +4,73 @@ const audio = document.querySelector("audio");
 const keyboard = document.querySelector(".keyboard");
 
 let lowerMode = false;
+let switchMode = false;
+
+window.addEventListener("keydown", kbdHandler);
+
+const octaveCodes = [
+  ["C1", "Db1", "D4"]
+]
+
+
+function switchBasicMode(e) {
+  console.log(e.keyCode);
+  switch(e.keyCode) {
+    case 33:
+    case 64:
+    case 35:
+    case 36:
+    case 37:
+    case 94:
+    case 38:
+    case 42:
+    case 40: {
+      console.log("SWITCHING");
+      console.log(e.code.charAt(5));
+      const targetOctave = e.code.charAt(5) - 1;
+      const targetNum = e.code.charAt(5);
+      console.log(octaveCodes[targetOctave]);
+
+      const firstOctaveElems = document.querySelectorAll(".keyboard--main .key");
+      firstOctaveElems.forEach(key => {
+        console.log(key.dataset.sound);
+        if (key.dataset.sound) {
+          key.dataset.sound = `${key.dataset.sound.slice(0, -1)}${targetNum}`;
+        }
+        console.log(key.dataset.sound);
+      })
+      console.log(firstOctaveElems);  
+      break;
+    }
+  }
+}
+
+//33, 64, 35, 36, 37, 94, 38, 42, 40 
+//keycodes of 1-9 with shift
+//49-57
 
 window.addEventListener("keypress", playSound);
 
 function kbdHandler(e) {
-  console.log(e.keyCode);
+  //console.log(e.keyCode);
   switch(e.keyCode) {
     case 16: {
       modeHandler(e);
       break;
     }
-    default: playSound(e);
+    default: break; playSound(e);
   }
 }
 
 function modeHandler(e) {
-  lowerMode = true;
-  console.log({lowerMode});
+  switchMode = true;
+  console.log({switchMode});
   window.addEventListener("keyup", function(e) {
     console.log("up", e.keyCode);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    key.classList.remove("key--pressing");
+    //const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    //key.classList.remove("key--pressing");
+    switchMode = false;
+    console.log({switchMode});
   })
 }
 
@@ -38,6 +84,10 @@ newAudio.forEach(audioElem => {
 
 
 function playSound(e) {
+  if (switchMode === true) {
+    switchBasicMode(e);
+    return;
+  }
   console.log(e.keyCode);
 
   let isPlaying = false;
