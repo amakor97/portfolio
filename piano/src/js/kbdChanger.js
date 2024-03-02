59116f72d51f68b94f669309a4de84ef39c65649
amakor97 <amakor.andrei@gmail.com>
2024-03-02 16:21:04 +0300

    [STYLE] piano: styled double-row mode

--- piano/src/js/kbdChanger.js
+++ piano/src/js/kbdChanger.js
@@ -21,29 +21,29 @@ function toggleVisualMode() {
 }
 
 function changeStylesForOneRow() {
-  kbdCont.classList.remove("keyboard-grid--double-rows");
-  kbdCont.classList.add("keyboard-grid--single-row");
+  kbdCont.classList.remove("keyboard-cont--double-rows");
+  kbdCont.classList.add("keyboard-cont--single-row");
 
-  leftPart.classList.remove("keyboard-grid__item--left-double");
-  leftPart.classList.add("keyboard-grid__item--left-single");
+  leftPart.classList.remove("keyboard-cont__item--left-double");
+  leftPart.classList.add("keyboard-cont__item--left-single");
 
-  rightPart.classList.remove("keyboard-grid__item--right-double");
-  rightPart.classList.add("keyboard-grid__item--right-single");
+  rightPart.classList.remove("keyboard-cont__item--right-double");
+  rightPart.classList.add("keyboard-cont__item--right-single");
 
-  centerPart.classList.remove("keyboard-grid__item--full-double");
-  centerPart.classList.add("keyboard-grid__item--full-single");
+  centerPart.classList.remove("keyboard-cont__item--full-double");
+  centerPart.classList.add("keyboard-cont__item--full-single");
 }
 
 function changeStylesForTwoRows() {
-  kbdCont.classList.remove("keyboard-grid--single-row");
-  kbdCont.classList.add("keyboard-grid--double-rows");
+  kbdCont.classList.remove("keyboard-cont--single-row");
+  kbdCont.classList.add("keyboard-cont--double-rows");
 
-  leftPart.classList.remove("keyboard-grid__item--left-single");
-  leftPart.classList.add("keyboard-grid__item--left-double");
+  leftPart.classList.remove("keyboard-cont__item--left-single");
+  leftPart.classList.add("keyboard-cont__item--left-double");
 
-  rightPart.classList.remove("keyboard-grid__item--right-single");
-  rightPart.classList.add("keyboard-grid__item--right-double");
+  rightPart.classList.remove("keyboard-cont__item--right-single");
+  rightPart.classList.add("keyboard-cont__item--right-double");
 
-  centerPart.classList.remove("keyboard-grid__item--full-single");
-  centerPart.classList.add("keyboard-grid__item--full-double");
+  centerPart.classList.remove("keyboard-cont__item--full-single");
+  centerPart.classList.add("keyboard-cont__item--full-double");
 }
