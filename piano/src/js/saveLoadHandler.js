9797c7c6c0c1d639c845aa1bfd6dcd2209ede1ca
amakor97 <amakor.andrei@gmail.com>
2024-04-29 21:50:34 +0300

    [FEAT] piano: update save and read from local storage and add saving and reading from file

--- piano/src/js/saveLoadHandler.js
+++ piano/src/js/saveLoadHandler.js
@@ -7,17 +7,14 @@ import { setBasicOffset, setAdvancedModeLayouts, setProModeLayouts,
 const saveToLsBtn = document.querySelector(".js-save-local-storage");
 const readFromLsBtn = document.querySelector(".js-read-local-storage");
 
-saveToLsBtn.addEventListener("click", saveToLs);
+window.onbeforeunload = saveToLs;
 function saveToLs() {
-  console.log(activeBasicOffset);
-  console.log(advancedModeLayouts);
-  console.log(proModeLayouts);
   localStorage.setItem("basic", activeBasicOffset);
   localStorage.setItem("adv", JSON.stringify(advancedModeLayouts));
   localStorage.setItem("pro", JSON.stringify(proModeLayouts));
 }
 
-readFromLsBtn.addEventListener("click", readFromLs);
+window.onload = readFromLs;
 function readFromLs() {
   setBasicOffset(localStorage.getItem("basic"));
   setAdvancedModeLayouts(JSON.parse(localStorage.getItem("adv")));
@@ -35,14 +32,31 @@ function saveToFile() {
   tmpObj.activeBasicOffset = activeBasicOffset;
   tmpObj.advancedModeLayouts = advancedModeLayouts;
   tmpObj.proModeLayouts = proModeLayouts;
-  console.log(tmpObj);
 
   let tmpString = JSON.stringify(tmpObj);
-  console.log(tmpString);
-
   let tmpLink = document.createElement("a");
   tmpLink.href = window.URL.createObjectURL(new Blob([tmpString], 
     {type: "application/json"}));
-  tmpLink.download = "SimplePianoLayouts.txt";
+  tmpLink.download = "SimplePianoLayouts.json";
   tmpLink.click();
+}
+
+readFromFileBtn.addEventListener("change", readFromFile);
+function readFromFile() {
+  const file = readFromFileBtn.files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    let fileString = reader.result;
+    let jsonObj = JSON.parse(fileString);
+
+    setBasicOffset(jsonObj.activeBasicOffset);
+    setAdvancedModeLayouts(jsonObj.advancedModeLayouts);
+    setProModeLayouts(jsonObj.proModeLayouts);
+    updateMode();
+  })
+
+  if (file) {
+    reader.readAsText(file);
+  }
 }
