6fad8e4f13253406239e26d81ea9a7937c324d13
amakor97 <amakor.andrei@gmail.com>
2024-05-09 16:43:41 +0300

    [REFACTOR] piano: update and test click switch mode functions

--- piano/src/js/clickFuncModeSwitcher.js
+++ piano/src/js/clickFuncModeSwitcher.js
@@ -3,14 +3,8 @@
 
 import { isEditModeActive, pressedOctave, switchModeType, 
   updateBasicSounds} from "./functionalModeSwitcher.js";
-import { updateVisualHints } from "./hintsUpdater.js";
-import { setBasicOffset, advancedModeLayouts, activeAdvancedLayout, 
-  proModeLayouts, activeProLayout, getOctaveClassByElem, 
-  updateAdvancedOctaveSounds,
-  switchBasicMode, updateAdvancedLayoutAndOctave2,
-  prevOctaveNum, setPrevOctaveNum, setPressedOctaveName, switchProMode3,
-  switchAdvancedMode2, updateBasicMode, updateAdvancedMode, 
-  updateProMode } from "./functionalModeSwitcher.js";
+import { getOctaveClassByElem, setPressedOctaveName, updateBasicMode, 
+  updateAdvancedMode, updateProMode } from "./functionalModeSwitcher.js";
 
 const allKeyElems = document.querySelectorAll(".key");
 allKeyElems.forEach(key => {
@@ -35,14 +29,13 @@ function switchByClick(e, key) {
       break;
     }
     case("pro"): {
-      switchProModeClick(key);
+      switchProModeClickHandler(key);
       break;
     }
   }
 }
 
 
-//export let prevOctaveNum = undefined;
 export let clickedProKeyElem = undefined;
 
 
@@ -54,128 +47,24 @@ function switchBasicModeClickHandler(e) {
 }
 
 
-function getPrevOctaveNum(key) {
-  console.log(key.parentNode.parentNode);
-
-  let prevOctave = key.parentNode.parentNode;
-  console.log(prevOctave);
-
-  let prevOctaveKeys = prevOctave.querySelectorAll(".key");
-  //console.log(prevOctaveKeys);
-
-  for (let keyElem of prevOctaveKeys) {
-    let kdbHint = keyElem.querySelector(".js-kbd-key-hint");
-    if (keyElem.dataset && (kdbHint.textContent !== "")) {
-      setPrevOctaveNum(Array.from(prevOctave.classList).find(
-        className => className.startsWith("keyboard--count")).slice(-1));
-        console.log("exit");
-        return prevOctaveNum;
-      }
-  }
-
-  console.log("marl");
-
-  let isZeroOctave = true;
-
-  const allKeyElems = document.querySelectorAll(".key");
-  allKeyElems.forEach(keyElem => {
-    let kbdHint = keyElem.querySelector(".js-kbd-key-hint"); ///??
-    if (kbdHint.textContent === "]") {
-      isZeroOctave = false;
-    }
-  })
-
-  console.log({isZeroOctave});
-
-  if (isZeroOctave) {
-    setPrevOctaveNum(0);
-    return "0";
-  }
-
-  let rbKey = document.querySelector(".key[data-symbol=']']");
-  let rbHint = rbKey.querySelector(".js-piano-key-hint");
-
-  let kdbHint = key.querySelector(".js-kbd-key-hint");
-  if (key.dataset && (kdbHint.textContent !== "")) {
-    setPrevOctaveNum(Array.from(key.parentNode.parentNode.classList).find(
-      className => className.startsWith("keyboard--count")).slice(-1));
-  }
-  return prevOctaveNum; 
-}
-
-
-function getKbdHint(prevOctaveNum) {
-  let prevOctaveKeys = document.querySelectorAll(
-    `.keyboard--count-${prevOctaveNum} .key:not(.key--empty)`);
-  let kbdHint = "";
-  prevOctaveKeys.forEach(prevOctaveKey => {
-    let tmpKbdHint = prevOctaveKey.querySelector(".js-kbd-key-hint");
-    if (tmpKbdHint.textContent) {
-      kbdHint = tmpKbdHint;
-    }
-  })
-
-  return kbdHint;
-}
-
-
 function getOctaveClassByHint(kbdHint) {
-
-  console.log("+++++++++++++++++++++++++++++++++++++++++++++++");
-  console.log(kbdHint.parentNode);
-
   let clickedOctave = kbdHint.parentNode.parentNode.parentNode;
-
-  console.log(clickedOctave.classList);
-
-  let clickedOctaveNum = Array.from(clickedOctave.classList).find(
-    className => className.startsWith("keyboard--count"));
-  console.log(clickedOctaveNum);
-
-  clickedOctave = document.querySelector(`.${clickedOctaveNum}`);
-
   let clickedOctaveKeys = clickedOctave.querySelectorAll(".key");
-
   let targetKeyElem = undefined;
 
   for (let keyElem of clickedOctaveKeys) {
     let tmpKbdHint = keyElem.querySelector(".js-kbd-key-hint");
     if (tmpKbdHint.textContent) {
-      console.log("found");
 
       for (let keyElem2 of allKeyElems) {
         if (tmpKbdHint.textContent === keyElem2.dataset.symbol) {
-          targetKeyElem = keyElem2; ////////////////////////
+          targetKeyElem = keyElem2; ///
         }
       }
-
-      //targetKeyElem = tmpKbdHint.parentNode;
     }
   }
-
-
-  if (targetKeyElem) {
-    console.log("ttg");
-    return getOctaveClassByElem(targetKeyElem);
-  }
-
-
-  for (let keyElem of allKeyElems) {
-    if (keyElem.dataset.symbol === kbdHint.textContent) {
-      return getOctaveClassByElem(keyElem);
-    }
-  }
-
-  for (let keyElem of allKeyElems) {
-    let tmpKbdHint = keyElem.querySelector(".js-kbd-key-hint");
-    if (tmpKbdHint.textContent === "]") {
-      console.log("rb is found");
-      return;
-    }
-  }
-
   
-  return "js-key-super";
+  return getOctaveClassByElem(targetKeyElem);
 }
 
 
@@ -194,35 +83,6 @@ function switchAdvancedModeClickHandler(e, key) {
 
 
 
-function switchAdvancedModeClick(e, key) {
-  let clickedKeyElem = e.target;
-
-  if (!pressedOctave) {
-    let tmpKbdHint = key.querySelector(".js-kbd-key-hint");
-
-    allKeyElems.forEach(keyElem => {
-      if (keyElem.dataset.key) {
-        let keyCo = keyElem.dataset.key;
-        if (tmpKbdHint.textContent === keyCo.slice(-1).toLowerCase()) {
-          console.log(keyElem.classList); //////
-        }
-      }
-    })
-
-    setPressedOctaveName(getOctaveClassByHint(tmpKbdHint));
-  } else {
-
-    let nextOctaveNum = Array.from(
-      clickedKeyElem.parentNode.parentNode.classList).find(
-      className => className.startsWith("keyboard--count"));
-
-    nextOctaveNum = nextOctaveNum.slice(-1);
-    updateAdvancedLayoutAndOctave2(pressedOctave, nextOctaveNum);
-
-    setPressedOctaveName(undefined);
-  }
-}
-
 
 function getClickedProKeyElem(key) {
   let kbdHint = key.querySelector(".js-kbd-key-hint");
@@ -236,17 +96,6 @@ function getClickedProKeyElem(key) {
 }
 
 
-function switchProModeClick(key) {
-  let noteForProMode = key.dataset.display;
-
-  if (!clickedProKeyElem) {
-    clickedProKeyElem = getClickedProKeyElem(key);
-  } else {
-    let targetKey = clickedProKeyElem.dataset.key;
-    switchProMode3(clickedProKeyElem, targetKey, noteForProMode);
-    clickedProKeyElem = undefined;
-  }
-}
 
 
 function switchProModeClickHandler(key) {
