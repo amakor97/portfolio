c8e935d71d2a81cae8441f3843e38a4f300a6610
amakor97 <amakor.andrei@gmail.com>
2024-06-06 21:36:53 +0300

    [UPDATE] piano: add 'about' modal

--- piano/src/js/dialogHandlers.js
+++ piano/src/js/dialogHandlers.js
@@ -5,15 +5,22 @@ const helpDialogToggleBtn = document.querySelector(
   ".js-dialog-help-toggle-btn");
 const helpDialogCloseBtn = document.querySelector(
   ".js-dialog-help-close-btn");
+
 const appDialog = document.querySelector(".js-dialog-app");
 const appDialogToggleBtn = document.querySelector(
   ".js-dialog-app-toggle-btn");
 const appDialogCloseBtn = document.querySelector(
   ".js-dialog-app-close-btn");
 
+const aboutDialog = document.querySelector(".js-dialog-about");
+const aboutDialogToggleBtn = document.querySelector(
+  ".js-dialog-about-toggle-btn");
+const aboutDialogCloseBtn = document.querySelector(
+  ".js-dialog-about-close-btn");
 
 let isHelpModalShowed = false;
 let isAppModalShowed = false;
+let isAboutModalShowed = false;
 
 helpDialogToggleBtn.addEventListener("click", function() {
   if (isHelpModalShowed) {
@@ -50,4 +57,23 @@ appDialogCloseBtn.addEventListener("click", () => {
   appDialogToggleBtn.classList.remove("menu__btn--toggled");
   appDialog.open = false;
   isAppModalShowed = false;
+})
+
+
+aboutDialogToggleBtn.addEventListener("click", function() {
+  if (isAboutModalShowed) {
+    aboutDialogToggleBtn.classList.remove("menu__btn--toggled");
+    aboutDialog.open = false;
+  } else {
+    aboutDialogToggleBtn.classList.add("menu__btn--toggled");
+    aboutDialog.open = true;
+  }
+  
+  isAboutModalShowed = !isAboutModalShowed;
+})
+
+aboutDialogCloseBtn.addEventListener("click", function() {
+  aboutDialogToggleBtn.classList.remove("menu__btn--toggled");
+  aboutDialog.open = false;
+  isAboutModalShowed = false;
 })
