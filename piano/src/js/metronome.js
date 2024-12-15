9c63722358ce7dcad07578d549973e5cfa3081e5
amakor97 <amakor.andrei@gmail.com>
2024-12-15 15:38:26 +0300

    finish metronome functionality

--- piano/src/js/metronome.js
+++ piano/src/js/metronome.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const playIcon = `
+  <svg
+    fill="none"
+    height="24"
+    width="24"
+    viewBox="0 0 24 24">
+    <polygon points="4.5,2 21,12 4.5,22" stroke="currentColor" stroke-width="3" stroke-linejoin="round" />
+  </svg>`;
+
+const stopIcon = `
+  <svg
+    fill="currentColor"
+    height="24"
+    width="24"
+    viewBox="0 0 24 24">
+    <rect width="18" height="18" x="3" y="3" rx="4" ry="4" />
+  </svg>`;
+
 const metronomeAudioFile = document.querySelector(".js-metronome-audiofile");
 const plusBtn = document.querySelector(".js-metronome-plus-btn");
 const minusBtn = document.querySelector(".js-metronome-minus-btn");
@@ -8,13 +26,22 @@ const bpmCont = document.querySelector(".js-metronome-bpm-number");
 
 let targetBpm = 60;
 let int = undefined;
+let isMetronomeRunning = false;
 
 
 startStopBtn.addEventListener("click", function() {
+  isMetronomeRunning = !isMetronomeRunning;
+  
   updateInterval();
   if (!int) {
     metronomeAudioFile.pause();
     metronomeAudioFile.currentTime = 0;
+  }
+
+  if (isMetronomeRunning) {
+    startStopBtn.innerHTML = stopIcon;
+  } else {
+    startStopBtn.innerHTML = playIcon;
   }
 })
 
